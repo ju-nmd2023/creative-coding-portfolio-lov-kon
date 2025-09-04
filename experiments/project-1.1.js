c6e813969bf15d4fb2c8e8627c9d6a9ac134e542
lov-kon <149784438+lov-kon@users.noreply.github.com>
2025-09-04 09:12:22 +0200

    First recreation

---
+++ experiments/project-1.1.js
@@ -0,0 +1,57 @@
+function setup() {
+  createCanvas(1000, 1000);
+}
+
+const size = 100;
+const layers = 10;
+
+function getRandomValue(pos, variance) {
+  return pos + map(Math.random(), 0, 1, -variance, variance);
+}
+
+function drawLayers(x, y, size, layers) {
+  // const half = size / 2;
+  const variance = size / 20;
+  noFill();
+  // rectMode(CENTER);
+  for (let i = 0; i < layers; i++) {
+    if (Math.random() > 0.8) {
+      continue;
+    }
+    const s = (size / layers) * i;
+    const half = s / 2;
+    beginShape();
+    vertex(
+      getRandomValue(x - half, variance),
+      getRandomValue(y - half, variance)
+    );
+    vertex(
+      getRandomValue(x + half, variance),
+      getRandomValue(y - half, variance)
+    );
+    vertex(
+      getRandomValue(x + half, variance),
+      getRandomValue(y + half, variance)
+    );
+    vertex(
+      getRandomValue(x - half, variance),
+      getRandomValue(y + half, variance)
+    );
+    endShape(CLOSE);
+    // rect(x - half, y - half, s, s);
+  }
+}
+
+function draw() {
+  background(255, 255, 255);
+
+  // drawLayers(100, 100, size, layers);
+  for (let y = 0; y < 10; y++) {
+    for (let x = 0; x < 10; x++) {
+      drawLayers(size / 2 + x * size, size / 2 + y * size, size, layers);
+    }
+  }
+
+  noLoop();
+}
+
