89b5b29ff5ce442a7787431ddd5cf905f6d9be0d
lov-kon <149784438+lov-kon@users.noreply.github.com>
2025-09-22 15:15:11 +0200

    start week 4, add noise

--- experiments/project-4.js
+++ experiments/project-4.js
@@ -5,30 +5,18 @@ let acceleration;
 function setup() {
   createCanvas(innerWidth, innerHeight);
   position = createVector(100, 100);
-  velocity = createVector(5, 8);
-  background(255);
-
+  velocity = createVector(0, 80);
+  background(250, 200, 220);
 }
 
 function draw() {
   noStroke();
-
-  push();
-  fill(200, random(255), 100);
-  ellipse(position.x, position.y, random(6));
-  ellipse(width - position.x, height - position.y, random(6));
-  pop();
-
-  push();
-  fill(80, random(255), 200);
-  ellipse(position.x, height - position.y, random(6));
-  ellipse(width - position.x, position.y, random(6));
-  pop();
+  fill(200, random(250), 200, 80);
+  ellipse(position.x, position.y, 10);
 
   if (position.x > width || position.x < 0) {
     velocity.x *= -1;
   }
-
   if (position.y > height || position.y < 0) {
     velocity.y *= -1;
   }
@@ -36,7 +24,7 @@ function draw() {
   const mouse = createVector(mouseX, mouseY);
   acceleration = p5.Vector.sub(mouse, position);
   acceleration.normalize();
-  acceleration.mult(0.5);
+  acceleration.mult(0.9);
 
   velocity.add(acceleration);
   velocity.limit(10);
