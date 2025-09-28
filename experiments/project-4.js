2a270b08f5de4a109b29e8f0cad027dc08cffc64
lov-kon <149784438+lov-kon@users.noreply.github.com>
2025-09-28 18:39:27 +0200

    beginning of tone.js

--- experiments/project-4.js
+++ experiments/project-4.js
@@ -3,7 +3,7 @@ let velocity;
 let acceleration;
 
 function setup() {
-  createCanvas(innerWidth, innerHeight);
+  createCanvas(window.innerWidth, window.innerHeight);
   position = createVector(100, 100);
   velocity = createVector(0, 80);
   background(250, 200, 220);
@@ -14,19 +14,33 @@ function draw() {
   fill(200, random(250), 200, 80);
   ellipse(position.x, position.y, 10);
 
-  if (position.x > width || position.x < 0) {
-    velocity.x *= -1;
-  }
-  if (position.y > height || position.y < 0) {
-    velocity.y *= -1;
-  }
+  if (position.x > width || position.x < 0) velocity.x *= -1;
+  if (position.y > height || position.y < 0) velocity.y *= -1;
 
-  const mouse = createVector(mouseX, mouseY);
-  acceleration = p5.Vector.sub(mouse, position);
-  acceleration.normalize();
-  acceleration.mult(0.9);
+  const mouseVec = createVector(mouseX, mouseY);
+  acceleration = p5.Vector.sub(mouseVec, position);
+  acceleration.normalize().mult(0.9);
 
-  velocity.add(acceleration);
-  velocity.limit(10);
+  velocity.add(acceleration).limit(10);
   position.add(velocity);
+}
+
+function mousePressed() {
+  console.log("Mouse pressed!");
+
+  Tone.start().then(() => {
+    console.log("AudioContext started!");
+
+    // Create filters **after AudioContext is started**
+    const lowpass = new Tone.Filter(500, "lowpass").toDestination();
+    const highpass = new Tone.Filter(500, "highpass").connect(lowpass);
+
+    // Create oscillator and connect it to filters
+    const osc = new Tone.Oscillator(Math.random() * 880 + 110, "sine").connect(
+      highpass
+    );
+
+    osc.start();
+    osc.stop("+0.5"); // stop after 0.2 seconds
+  });
 }
